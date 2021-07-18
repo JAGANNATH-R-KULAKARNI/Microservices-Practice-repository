56899988d7f62e9990f96adc82f5d6e1a0eef95d
JAGANNATH R KUAKARNI <jagannathrkulakarni.171845@gmail.com>
2021-07-19 01:45:16 +0530

    added an efficient way of getting data by creating event bus and query

--- event_bus/index.js
+++ event_bus/index.js
@@ -2,15 +2,16 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const axios=require('axios');
 const PORT=4005;
+const cors=require('cors');
 const app=express();
 app.use(bodyParser.json());
 
-app.post('/events',(req,res)=>{
+app.post('/events',async (req,res)=>{
     const event=req.body;
-
-    axios.post('http://localhost:4000/events',event).then((u)=>console.log(u)).catch((err)=>console.log(err));
-    axios.post('http://localhost:4001/events',event).then((u)=>console.log(u)).catch((err)=>console.log(err));
-    axios.post('http://localhost:4002/events',event).then((u)=>console.log(u)).catch((err)=>console.log(err));
+  //console.log(event)
+   await axios.post('http://localhost:4000/events',event).then((u)=>console.log("u")).catch((err)=>console.log("err"));
+   await axios.post('http://localhost:4001/events',event).then((u)=>console.log("u")).catch((err)=>console.log("err"));
+   await axios.post('http://localhost:4002/events',event).then((u)=>console.log("u")).catch((err)=>console.log("err"));
 
     res.send({status : 'OK'});
 });
